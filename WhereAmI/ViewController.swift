8c36c6dd036f017642066f14d59a357f42f44f1d
Giovanni <giomadalozzo@gmail.com>
2021-12-03 17:51:19 -0300

    Tap gesture on annotations

--- WhereAmI/ViewController.swift
+++ WhereAmI/ViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     var coreLocationManager = CLLocationManager()
     var locationManager: LocationManager!
+    
+    var annotationList: [MKAnnotation] = []
+    var count: Int = 0
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var jokingLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     
     @IBAction func updateButton(_ sender: Any) {
+        mapView.removeAnnotations(annotationList)
+        getLocation()
+        self.count = 0
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         coreLocationManager.delegate = self
+        mapView.delegate = self
         locationManager = LocationManager.shared
         
         if coreLocationManager.authorizationStatus == CLAuthorizationStatus.notDetermined && coreLocationManager.responds(to: #selector(CLLocationManager.requestWhenInUseAuthorization)) {
@@ -47,14 +54,70 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let annotation = MKPointAnnotation()
         annotation.coordinate = locationPinCoord
         
+        annotationList = []
+        annotationList.append(annotation)
         mapView.addAnnotation(annotation)
-        mapView.showAnnotations([annotation], animated: true)
+        
+        for _ in 0...4{
+            let annotationAux = MKPointAnnotation()
+            annotationAux.coordinate.longitude = annotation.coordinate.longitude + Double.random(in: 0.001...0.005)
+            annotationAux.coordinate.latitude = annotation.coordinate.latitude + Double.random(in: 0.001...0.005)
+            mapView.addAnnotation(annotationAux)
+            annotationList.append(annotationAux)
+        }
+        mapView.showAnnotations(annotationList, animated: true)
+        
+        locationManager.getReverseGeoCodedLocation(location: CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)) { location, placemark, error in
+            if self.count == 0 {
+                self.locationLabel.text = "Hmmmm, your location is the red mark 🤠 Click on it to get more info"
+                self.count += 1
+            }else{
+                self.locationLabel.text = self.addressFormatter(placemark: placemark!)
+            }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status != CLAuthorizationStatus.notDetermined || status != CLAuthorizationStatus.denied || status != CLAuthorizationStatus.restricted {
             getLocation()
         }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        locationManager.getReverseGeoCodedLocation(location: CLLocation(latitude: view.annotation!.coordinate.latitude, longitude: view.annotation!.coordinate.longitude)) { location, placemark, error in
+            
+            self.locationLabel.text = self.addressFormatter(placemark: placemark!)
+            if self.locationLabel.text == "" {
+                self.locationLabel.text = "Sorry, but Idk where you at"
+            }
+        }
+    }
+    
+    func addressFormatter(placemark: CLPlacemark) -> String {
+        guard let thoroughfare = placemark.thoroughfare else {
+            return ""
+        }
+        
+        guard let subThoroughfare = placemark.subThoroughfare else {
+            return ""
+        }
+        
+        guard let subLocality = placemark.subLocality else {
+            return ""
+        }
+        
+        guard let locality = placemark.locality else {
+            return ""
+        }
+        
+        guard let administrativeArea = placemark.administrativeArea else {
+            return ""
+        }
+        
+        guard let country = placemark.country else {
+            return ""
+        }
+        return thoroughfare + ", " + subThoroughfare + ", " + subLocality + ", " + locality + " - " + administrativeArea + ", " + country
     }
 
 
